e21204e47c2811539f5c7173110f28e741251b1c
lamasu <rafaerthal@gmail.com>
2014-04-30 00:34:22 -0300

    fixing options route

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(methodOverride());
 app.options('/*', function (request, response) {
     response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Methods', 'GET');
-    response.header('Access-Control-Allow-Headers', '*');
+    response.header('Access-Control-Allow-Headers', 'auth-signature,auth-timestamp,auth-transactionid,x-http-method-override');
     response.end();
 });
 app.use(require('./lib/auth').signature);
