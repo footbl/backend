a8673cc5d48f0d60e0fbe868746d2b6e40805f65
lamasu <rafaerthal@gmail.com>
2014-05-03 12:06:07 -0300

    adding championship rounds count and current round

--- models/championship.js
+++ models/championship.js
@@ -65,15 +65,40 @@ schema = new Schema({
 });
 
 schema.plugin(require('mongoose-json-select'), {
-    'name'        : 1,
-    'picture'     : 1,
-    'year'        : 1,
-    'competitors' : 1,
-    'type'        : 1,
-    'country'     : 1
+    'name'         : 1,
+    'picture'      : 1,
+    'year'         : 1,
+    'competitors'  : 1,
+    'type'         : 1,
+    'country'      : 1,
+    'rounds'       : 1,
+    'currentRound' : 1
 });
 
 schema.index({'name' : 1, 'country' : 1}, {'unique' : true});
+
+/**
+ * @callback
+ * @summary Setups createdAt and updatedAt
+ *
+ * @param next
+ *
+ * @since 2013-03
+ * @author Rafael Almeida Erthal Hermano
+ */
+schema.pre('init', function (next) {
+    'use strict';
+
+    var query;
+    query = require('./match').find();
+    query.where('championship').equals(this._id);
+    query.populate('guest');
+    query.populate('host');
+    query.exec(function (error, matches) {
+        this.matches = matches || [];
+        next(error);
+    }.bind(this));
+});
 
 /**
  * @callback
@@ -93,6 +118,42 @@ schema.pre('save', function (next) {
         this.updatedAt = new Date();
     }
     next();
+});
+
+/**
+ * @method
+ * @summary Return championship rounds
+ *
+ * @since 2013-03
+ * @author Rafael Almeida Erthal Hermano
+ */
+schema.virtual('rounds').get(function () {
+    'use strict';
+
+    if (!this.matches) { return 1; }
+
+    return this.matches.map(function (match) {
+        return match.round;
+    }).sort().pop();
+});
+
+/**
+ * @method
+ * @summary Return championship rounds
+ *
+ * @since 2013-03
+ * @author Rafael Almeida Erthal Hermano
+ */
+schema.virtual('currentRound').get(function () {
+    'use strict';
+
+    if (!this.matches) { return 1; }
+
+    return this.matches.filter(function (match) {
+        return match.finished;
+    }).map(function (match) {
+        return match.round;
+    }).sort().pop();
 });
 
 module.exports = mongoose.model('Championship', schema);
