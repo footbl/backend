a24a5d5056c8c4e40b2997d65c86908ef92a4388
lamasu <rafaerthal@gmail.com>
2014-05-30 15:51:58 -0300

    debbuging group ranking

--- workers/group-ranking.js
+++ workers/group-ranking.js
@@ -34,6 +34,10 @@ query.exec(function (error, groups) {
         if (!group.championship.roundFinished) { return next(); }
         round = group.championship.currentRound - 1;
 
+        console.log(group.members);
+        console.log(group.members.filter(function (member) {
+            return member.username || member.facebookId || member.email;
+        }));
         return async.sortBy(group.members.filter(function (member) {
             return member.username || member.facebookId || member.email;
         }), function (member, next) {
