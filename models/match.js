780db0d837bd71703499267451813de1116f6451
lamasu <rafaerthal@gmail.com>
2014-06-01 19:31:41 -0300

    logging match update

--- models/match.js
+++ models/match.js
@@ -171,9 +171,9 @@ schema.pre('save', function (next) {
 
                 bet.finished = this.finished;
                 bet.reward   = bet.result === this.winner ? this.reward * bet.bid : 0;
-                return wallet.save(function () {
-                    next();
-                });
+
+                console.log(wallet.user, bet.finished, bet.reward, bet._id);
+                return wallet.save(function () { next(); });
             }.bind(this));
         }.bind(this), next);
     }.bind(this));
