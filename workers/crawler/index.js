384e291b6a9ecb819aa4307b4a21f6ad8cc6e356
Lamasu <rafaerthal@gmail.com>
2014-06-29 14:27:21 -0300

    repeting crawler 20x

--- workers/crawler/index.js
+++ workers/crawler/index.js
@@ -17,96 +17,100 @@ Championship = require('../../models/championship');
 Match        = require('../../models/match');
 Team         = require('../../models/team');
 
-async.seq(function (next) {
-    teamApi.find(next);
-}, function (teams, next) {
-    async.each(teams, function (team, next) {
-        Team.update({'name' : team.name}, {'name' : team.name, 'picture' : team.logo}, {'upsert' : true}, next);
-    }, next);
-}, function (next) {
-    Championship.findOneAndUpdate({
-        'name'    : "World Cup"
-    }, {'$set' : {
-        'country' : "International",
-        'edition' : 2014,
-        'name'    : "World Cup",
-        'picture' : "http://res.cloudinary.com/hivstsgwo/image/upload/v1403968689/world_icon_2x_frtfue.png",
-        'type'    : "world cup"
-    }}, {
-        'upsert' : true
-    }, function (error, champ) {
-        championship = champ;
-        next();
-    });
-}, function (next) {
-    async.parallel([matchApi.findPreGame, matchApi.findInProgress, matchApi.findInFinal], next);
-}, function (results, next) {
-    async.reduce(results, [], function (memo, item, next) {
-        next(null, memo.concat(item));
-    }, next);
-}, function (matches, next) {
-    async.map(matches, function (match, next) {
-        teamApi.findById(match.awayTeamId, function (error, team) {
-            if (error) { next(error); }
-            Team.findOne({'name' : team.name}, function (error, team) {
-                if (error) { next(error); }
-                match.guest = team ? team._id : null;
-                next(null, match);
-            });
-        });
-    }, next);
-}, function (matches, next) {
-    async.map(matches, function (match, next) {
-        teamApi.findById(match.homeTeamId, function (error, team) {
-            if (error) { next(error); }
-            Team.findOne({'name' : team.name}, function (error, team) {
-                if (error) { next(error); }
-                match.host = team ? team._id : null;
-                next(null, match);
-            });
-        });
-    }, next);
-}, function (matches, next) {
-    async.map(matches, function (match, next) {
-        match.championship = championship._id;
-        next(null, match);
-    }, next);
-}, function (matches, next) {
-    var guestRound;
-    guestRound = {};
-    matches.sort(function (a, b) {
-        if (a.startTime > b.startTime) return 1;
-        if (a.startTime < b.startTime) return -1;
-        return 0;
-    }).forEach(function (match) {
-        if (!guestRound[match.guest]) { guestRound[match.guest] = 0; }
-        if (!guestRound[match.host]) { guestRound[match.host] = 0; }
-        guestRound[match.guest] += 1;
-        guestRound[match.host] += 1;
-        match.round = guestRound[match.guest > match.host ? match.guest : match.host];
-    });
-    next(null, matches);
-}, function (matches, next) {
-    async.each(matches, function (match, next) {
-        Match.update({
-            'guest'        : match.guest,
-            'host'         : match.host,
-            'round'        : match.round,
-            'championship' : match.championship
-        }, {
-            'guest'        : match.guest,
-            'host'         : match.host,
-            'round'        : match.round,
-            'championship' : match.championship,
-            'elapsed'      : match.currentGameMinute,
-            'date'         : match.startTime,
-            'finished'     : match.status === 'Final',
-            'result'       : {
-                'guest' : match.awayScore,
-                'host'  : match.homeScore
-            }
-        }, {
-            'upsert' : true
+async.timesSeries(20, function (n, next) {
+    async.seq(function (next) {
+        teamApi.find(next);
+    }, function (teams, next) {
+        async.each(teams, function (team, next) {
+            Team.update({'name' : team.name}, {'name' : team.name, 'picture' : team.logo}, {'upsert' : true}, next);
         }, next);
-    }, next);
-}, process.exit)();
+    }, function (next) {
+        Championship.findOneAndUpdate({
+            'name'    : "World Cup"
+        }, {'$set' : {
+            'country' : "International",
+            'edition' : 2014,
+            'name'    : "World Cup",
+            'picture' : "http://res.cloudinary.com/hivstsgwo/image/upload/v1403968689/world_icon_2x_frtfue.png",
+            'type'    : "world cup"
+        }}, {
+            'upsert' : true
+        }, function (error, champ) {
+            championship = champ;
+            next();
+        });
+    }, function (next) {
+        async.parallel([matchApi.findPreGame, matchApi.findInProgress, matchApi.findInFinal], next);
+    }, function (results, next) {
+        async.reduce(results, [], function (memo, item, next) {
+            next(null, memo.concat(item));
+        }, next);
+    }, function (matches, next) {
+        async.map(matches, function (match, next) {
+            teamApi.findById(match.awayTeamId, function (error, team) {
+                if (error) { next(error); }
+                Team.findOne({'name' : team.name}, function (error, team) {
+                    if (error) { next(error); }
+                    match.guest = team ? team._id : null;
+                    next(null, match);
+                });
+            });
+        }, next);
+    }, function (matches, next) {
+        async.map(matches, function (match, next) {
+            teamApi.findById(match.homeTeamId, function (error, team) {
+                if (error) { next(error); }
+                Team.findOne({'name' : team.name}, function (error, team) {
+                    if (error) { next(error); }
+                    match.host = team ? team._id : null;
+                    next(null, match);
+                });
+            });
+        }, next);
+    }, function (matches, next) {
+        async.map(matches, function (match, next) {
+            match.championship = championship._id;
+            next(null, match);
+        }, next);
+    }, function (matches, next) {
+        var guestRound;
+        guestRound = {};
+        matches.sort(function (a, b) {
+            if (a.startTime > b.startTime) return 1;
+            if (a.startTime < b.startTime) return -1;
+            return 0;
+        }).forEach(function (match) {
+                if (!guestRound[match.guest]) { guestRound[match.guest] = 0; }
+                if (!guestRound[match.host]) { guestRound[match.host] = 0; }
+                guestRound[match.guest] += 1;
+                guestRound[match.host] += 1;
+                match.round = guestRound[match.guest > match.host ? match.guest : match.host];
+            });
+        next(null, matches);
+    }, function (matches, next) {
+        async.each(matches, function (match, next) {
+            Match.update({
+                'guest'        : match.guest,
+                'host'         : match.host,
+                'round'        : match.round,
+                'championship' : match.championship
+            }, {
+                'guest'        : match.guest,
+                'host'         : match.host,
+                'round'        : match.round,
+                'championship' : match.championship,
+                'elapsed'      : match.currentGameMinute,
+                'date'         : match.startTime,
+                'finished'     : match.status === 'Final',
+                'result'       : {
+                    'guest' : match.awayScore,
+                    'host'  : match.homeScore
+                }
+            }, {
+                'upsert' : true
+            }, next);
+        }, next);
+    }, function () {
+        setTimeout(next, 30000);
+    })();
+}, process.exit);
