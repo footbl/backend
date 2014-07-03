b50c42355f765b7366b0774afd4e6dcba889c817
Lamasu <rafaerthal@gmail.com>
2014-07-03 14:55:43 -0300

    championship ranking

--- workers/ranking/championship.js
+++ workers/ranking/championship.js
@@ -34,7 +34,7 @@ query.exec(function (error, championships) {
         query.populate('bets.match');
         return query.exec(function (error, wallets) {
             async.sortBy(wallets.filter(function (wallet) {
-                return wallet.user && (wallet.user.username || wallet.user.facebookId || wallet.user.email);
+                return wallet.user && (wallet.user.name || wallet.user.facebookId || wallet.user.email);
             }), function (wallet, next) {
                 while (wallet.rounds.length <= round) {
                     wallet.rounds.push({ranking : Infinity, funds : 100});
