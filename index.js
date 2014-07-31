9920f303e708a0ff09d537247833c870c70cf8c3
Lamasu <rafaerthal@gmail.com>
2014-07-31 17:46:37 -0300

    removing credit request from v1

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ app.use(require('./controllers/comment'));
 app.use(require('./controllers/match'));
 app.use(require('./controllers/team'));
 app.use(require('./controllers/user'));
-app.use(require('./controllers/credit-request'));
 app.use(require('./controllers/group'));
 app.get('/', function (request, response) { response.send(200, {'pageSize' : nconf.get('PAGE_SIZE')}); });
 app.listen(nconf.get('PORT'));
