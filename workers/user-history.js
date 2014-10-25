6237f3990e22be43beb2ff5a997f0dd18e142002
Lamasu <rafaerthal@gmail.com>
2014-10-25 08:40:46 -0200

    fixing user wallet history to include stake

--- workers/user-history.js
+++ workers/user-history.js
@@ -22,12 +22,12 @@ module.exports = function (next) {
       var emptyHistory, hasTodayHistory, changedFunds;
       emptyHistory = user.history.length === 0;
       hasTodayHistory = user.history[0] && user.history[0].date.getDate() === now.getDate() && user.history[0].date.getMonth() === now.getMonth() && user.history[0].date.getFullYear() === now.getFullYear();
-      changedFunds = user.history[0] && user.history[0].funds !== user.funds;
+      changedFunds = user.history[0] && user.history[0].funds !== user.funds + user.stake;
 
       if (emptyHistory || (!hasTodayHistory && changedFunds)) {
-        user.history.push({'date' : now, 'funds' : user.funds});
+        user.history.push({'date' : now, 'funds' : user.funds + user.stake});
       } else if (hasTodayHistory && changedFunds) {
-        user.history[0].funds = user.funds;
+        user.history[0].funds = user.funds + user.stake;
       }
       user.history = user.history.sort(function (a, b) {
         return b.date - a.date;
