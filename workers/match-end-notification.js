ee2f593cc5fcb474267e19bc4d590609e5e4ba28
Lamasu <rafaerthal@gmail.com>
2014-11-15 00:06:54 -0200

    fixing match end notification

--- workers/match-end-notification.js
+++ workers/match-end-notification.js
@@ -19,23 +19,30 @@ nconf.env();
 nconf.defaults(require('../config'));
 
 module.exports = function (next) {
-  async.waterfall([function () {
+  async.waterfall([function (next) {
     var query;
     query = Championship.find();
     query.exec(next);
   }, function (championships, next) {
     async.filter(championships, function (championship, next) {
-      var query;
-      query = Match.find();
-      query.where('championship').equals(championship._id);
-      query.where('date').gte(today).lt(tomorrow);
-      query.sort('-date');
-      query.limit(1);
-      query.exec(next);
-    }, function (error, matches) {
-      var lastMatch;
-      lastMatch = matches[0];
-      next(!error && lastMatch.date - now > 0 && lastMatch.date - now < 1000 * 60 * 10);
+      async.waterfall([function (next) {
+        var query;
+        query = Match.find();
+        query.where('championship').equals(championship._id);
+        query.where('date').gte(today).lt(tomorrow);
+        query.sort('-date');
+        query.limit(1);
+        query.exec(next);
+      }, function (matches, next) {
+        var lastMatch, notify;
+        lastMatch = matches[0];
+        notify = !!lastMatch && lastMatch.date - now > 0 && lastMatch.date - now < 1000 * 60 * 10;
+        next(null, notify);
+      }], function (error, result) {
+        next(!error && result);
+      })
+    }, function (championships) {
+      next(null, championships);
     });
   }, function (championships, next) {
     async.each(championships, function (championship, next) {
